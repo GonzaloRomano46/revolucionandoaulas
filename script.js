2de5058fe901e26299347279180e6801a4901f0f
GonzaloRomano46 <131217885+GonzaloRomano46@users.noreply.github.com>
2024-10-14 17:22:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const iaImage = document.getElementById("ia-image");
+    iaImage.addEventListener("click", function() {
+        window.location.href = "https://www.instagram.com/educacion.ia_?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==";
+    });
+});
